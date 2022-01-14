808821c48c1ed844c18acac6ff2af67b882d339d
Leonardo Lopes <leolopesquermes1997@gmail.com>
2022-01-13 22:18:50 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/leodev/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leodev.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
